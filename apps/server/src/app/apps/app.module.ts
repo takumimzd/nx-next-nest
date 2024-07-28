66dd83962d9d9de8d080908a1fdf8f5ba0cb2a35
takumimzd <retnuh.takumi@gmail.com>
2024-07-28 16:14:45 +0900

    ファイル移動

--- apps/server/src/app/apps/app.module.ts
+++ apps/server/src/app/apps/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosController } from './todos/todos.controller';
-import { TodosService } from './todos/todos.service';
+import { TodosController } from '../todos/todos.controller';
+import { TodosService } from '../todos/todos.service';
 
 @Module({
   imports: [],
